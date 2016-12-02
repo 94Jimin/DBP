8362f66ade6212e0b25552f907b5d5756122f5be
Park Jimin <94jimin818@naver.com>
2016-12-02 16:35:38 +0900

    DB에 책 관한 data insert

--- sqlserverconn.sql
+++ sqlserverconn.sql
@@ -1,15 +1,87 @@
 create table MEMBER(
-member_id varchar(50) PRIMARY KEY NOT NULL,
-member_passwd varchar(50) NOT NULL,
-member_name varchar(10) NOT NULL,
-member_reg_date datetime NOT NULL,
-member_gender char(2) NOT NULL,
-member_favorite varchar(50)
+id varchar(50) PRIMARY KEY NOT NULL,
+passwd varchar(50) NOT NULL,
+name varchar(10) NOT NULL,
+reg_date datetime NOT NULL,
+gender char(2) NOT NULL,
+favorite varchar(50)
 );
 
 select * from MEMBER;
 
-drop table MEMBER;
+--drop table MEMBER;
 
-insert into MEMBER(member_id,member_passwd,member_name,member_reg_date,member_gender) 
+insert into MEMBER(id,passwd,name,reg_date,gender) 
 values('admin','1234','관리자',getdate(),'F');
+
+------------------------------------------------------------------------------------
+--book 테이블 관련 만들기
+create table MAIN_CATEGORY(
+code varchar(20) primary key not null,
+name varchar(20) not null
+);
+select * from MAIN_CATEGORY;
+
+--DROP TABLE MAIN_CATEGORY;
+
+create table SUB_CATEGORY(
+code varchar(20) primary key not null,
+name varchar(20) not null,
+main_category_code varchar(20) foreign key references MAIN_CATEGORY (code) 
+);
+select * from SUB_CATEGORY;
+
+--drop table SUB_CATEGORY;
+
+create table BOOK(
+code varchar(20) primary key not null,
+subject varchar(40) not null,
+writer varchar(40) not null,
+publisher varchar(20) not null,
+main_category_code varchar(20) foreign key references MAIN_CATEGORY (code),
+sub_category_code varchar(20) foreign key references SUB_CATEGORY (code)
+);
+select * from BOOK;
+
+--drop table BOOK;
+
+---------------------------------------------------------------------------------
+-- 카테고리 만들기
+
+insert into MAIN_CATEGORY(code,name) values('100','소설');
+insert into MAIN_CATEGORY(code,name) values('200','경영/경제');
+insert into MAIN_CATEGORY(code,name) values('300','자기계발');
+insert into MAIN_CATEGORY(code,name) values('400','여행');
+insert into MAIN_CATEGORY(code,name) values('500','컴퓨터/IT');
+
+
+insert into SUB_CATEGORY(code,name,main_category_code) values('100-A','한국소설','100');
+insert into SUB_CATEGORY(code,name,main_category_code) values('100-B','영미소설','100');
+insert into SUB_CATEGORY(code,name,main_category_code) values('100-C','판타지소설','100');
+
+insert into SUB_CATEGORY(code,name,main_category_code) values('200-A','경영일반','200');
+insert into SUB_CATEGORY(code,name,main_category_code) values('200-B','경제일반','200');
+insert into SUB_CATEGORY(code,name,main_category_code) values('200-C','CEO/리더십','200');
+
+insert into SUB_CATEGORY(code,name,main_category_code) values('300-A','성공/삶의자세','300');
+insert into SUB_CATEGORY(code,name,main_category_code) values('300-B','설득/화술/협상','300');
+insert into SUB_CATEGORY(code,name,main_category_code) values('300-C','인간간관계','300');
+
+insert into SUB_CATEGORY(code,name,main_category_code) values('400-A','국내여행','400');
+insert into SUB_CATEGORY(code,name,main_category_code) values('400-B','해외여행','400');
+
+insert into SUB_CATEGORY(code,name,main_category_code) values('500-A','IT비즈니스','500');
+insert into SUB_CATEGORY(code,name,main_category_code) values('500-B','개발/프로그래밍','500');
+insert into SUB_CATEGORY(code,name,main_category_code) values('500-C','컴퓨터/앱활용','500');
+
+---------------------------------------------------------------------------------------
+--book 만들기
+
+insert into BOOK(code,subject,writer,publisher,main_category_code,sub_category_code) 
+values('200-B-1','넛지','리처드 탈러, 캐스선스타인','리더북스','200','200-B');
+insert into BOOK(code,subject,writer,publisher,main_category_code,sub_category_code) 
+values('200-B-2','화폐전쟁','쑹훙빈','랜덤하우스코리아','200','200-B');
+insert into BOOK(code,subject,writer,publisher,main_category_code,sub_category_code) 
+values('200-B-1','그들이 말하지 않는 23가지','장하준','부키','200','200-B');
+insert into BOOK(code,subject,writer,publisher,main_category_code,sub_category_code) 
+values('200-B-1','경제학 콘서트','팀 하포드','웅진지식하우스','200','200-B');
