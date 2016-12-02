974ba36afcc1a7493021979e7ecabacbcb475989
Park Jimin <94jimin818@naver.com>
2016-12-03 02:21:00 +0900

    Update sqlserverconn.sql
    제발 충돌나지 말라고 ㅠㅜ

--- sqlserverconn.sql
+++ sqlserverconn.sql
@@ -1,4 +1,4 @@
-create table MEMBER(		  
+create table MEMBER(
 member_id varchar(50) PRIMARY KEY NOT NULL,
 member_passwd varchar(50) NOT NULL,
 member_name varchar(10) NOT NULL,
@@ -6,10 +6,10 @@ member_reg_date datetime NOT NULL,
 member_gender char(2) NOT NULL,
 member_favorite varchar(50)
 );
-  		  
+
 select * from MEMBER;
-  		  
+
 drop table MEMBER;
-  		  
+
 insert into MEMBER(member_id,member_passwd,member_name,member_reg_date,member_gender)
-values('admin','1234','관리자',getdate(),'F'); 
+values('admin','1234','°ü¸®ÀÚ',getdate(),'F');
