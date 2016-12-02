b87db267f143dc3c417d188f74b7738c9c1ecc46
Park Jimin <94jimin818@naver.com>
2016-12-03 02:20:09 +0900

    Update sqlserverconn.sql
    다시 to 충돌해결

--- sqlserverconn.sql
+++ sqlserverconn.sql
@@ -1,15 +1,15 @@
- create table MEMBER(		  
- member_id varchar(50) PRIMARY KEY NOT NULL,
- member_passwd varchar(50) NOT NULL,
- member_name varchar(10) NOT NULL,
- member_reg_date datetime NOT NULL,
- member_gender char(2) NOT NULL,
- member_favorite varchar(50)
-  );		  );
+create table MEMBER(		  
+member_id varchar(50) PRIMARY KEY NOT NULL,
+member_passwd varchar(50) NOT NULL,
+member_name varchar(10) NOT NULL,
+member_reg_date datetime NOT NULL,
+member_gender char(2) NOT NULL,
+member_favorite varchar(50)
+);
   		  
-  select * from MEMBER;		  select * from MEMBER;
+select * from MEMBER;
   		  
-  drop table MEMBER;		  drop table MEMBER;
+drop table MEMBER;
   		  
- insert into MEMBER(member_id,member_passwd,member_name,member_reg_date,member_gender) 		 +insert into MEMBER values('admin','$2a$10$QxYtHTU2SRnLC.g1g8.E6ucImfLpigsmCxNF3BiSyUU7gDnrkpf52','°ü¸®ÀÚ',getdate(),'M','');
- values('admin','1234','관리자',getdate(),'F'); 
+insert into MEMBER(member_id,member_passwd,member_name,member_reg_date,member_gender)
+values('admin','1234','관리자',getdate(),'F'); 
