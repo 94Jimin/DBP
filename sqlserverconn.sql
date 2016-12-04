1693c43f3eeee878a1ec17ffc90bcb5cf1a9e903
Park Jimin <94jimin818@naver.com>
2016-12-04 18:39:09 +0900

    db 수정

--- sqlserverconn.sql
+++ sqlserverconn.sql
@@ -1,3 +1,17 @@
+-- 테이블 리스트 
+select * from information_schema.tables;
+
+-- 컬럼 상세 정보 
+select * from information_schema.columns
+where table_name = 'SUB_CATEGORY'
+order by 5;
+
+--기본키 확인
+SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE 
+WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_NAME), 'ISPRIMARYKEY') = 1 AND TABLE_NAME = 'SUB_CATEGORY';
+
+-------------------------------------
+
 create table MEMBER(
 id varchar(50) PRIMARY KEY NOT NULL,
 passwd varchar(60) NOT NULL,
@@ -14,20 +28,20 @@ insert into MEMBER values('admin','$2a$10$QxYtHTU2SRnLC.g1g8.E6ucImfLpigsmCxNF3B
 ------------------------------------------------------------------------------------
 --book 테이블 관련 만들기
 create table MAIN_CATEGORY(
-code int identity(1, 1) primary key not null,
+code int primary key not null,
 name varchar(20) not null
 );
 select * from MAIN_CATEGORY;
-
+--alter table MAIN_CATEGORY alter column code int not null;
 --DROP TABLE MAIN_CATEGORY;
 
 create table SUB_CATEGORY(
-code int identity(1, 1) primary key not null,
+code int primary key not null,
 name varchar(20) not null,
 main_code int foreign key references MAIN_CATEGORY (code) 
 );
 select * from SUB_CATEGORY;
-
+--alter table SUB_CATEGORY alter column code int not null;
 --drop table SUB_CATEGORY;
 
 create table BOOK(
@@ -66,52 +80,52 @@ select * from LIST;
 ---------------------------------------------------------------------------------
 -- 카테고리 만들기
 
-insert into MAIN_CATEGORY(name) values('소설');
-insert into MAIN_CATEGORY(name) values('경영/경제');
-insert into MAIN_CATEGORY(name) values('자기계발');
-insert into MAIN_CATEGORY(name) values('여행');
-insert into MAIN_CATEGORY(name) values('컴퓨터/IT');
+insert into MAIN_CATEGORY(code,name) values(100,'소설');
+insert into MAIN_CATEGORY(code,name) values(200,'경영/경제');
+insert into MAIN_CATEGORY(code,name) values(300,'자기계발');
+insert into MAIN_CATEGORY(code,name) values(400,'여행');
+insert into MAIN_CATEGORY(code,name) values(500,'컴퓨터/IT');
 
 
-insert into SUB_CATEGORY(name,main_code) values('한국소설',1);
-insert into SUB_CATEGORY(name,main_code) values('영미소설',1);
-insert into SUB_CATEGORY(name,main_code) values('판타지소설',1);
+insert into SUB_CATEGORY(code,name,main_code) values(110,'한국소설',100);
+insert into SUB_CATEGORY(code,name,main_code) values(120,'영미소설',100);
+insert into SUB_CATEGORY(code,name,main_code) values(130,'판타지소설',100);
 
-insert into SUB_CATEGORY(name,main_code) values('경영일반',2);
-insert into SUB_CATEGORY(name,main_code) values('경제일반',2);
-insert into SUB_CATEGORY(name,main_code) values('CEO/리더십',2);
+insert into SUB_CATEGORY(code,name,main_code) values(210,'경영일반',200);
+insert into SUB_CATEGORY(code,name,main_code) values(220,'경제일반',200);
+insert into SUB_CATEGORY(code,name,main_code) values(230,'CEO/리더십',200);
 
-insert into SUB_CATEGORY(name,main_code) values('성공/삶의자세',3);
-insert into SUB_CATEGORY(name,main_code) values('설득/화술/협상',3);
-insert into SUB_CATEGORY(name,main_code) values('인간간관계',3);
+insert into SUB_CATEGORY(code,name,main_code) values(310,'성공/삶의자세',300);
+insert into SUB_CATEGORY(code,name,main_code) values(320,'설득/화술/협상',300);
+insert into SUB_CATEGORY(code,name,main_code) values(330,'인간간관계',300);
 
-insert into SUB_CATEGORY(name,main_code) values('국내여행',4);
-insert into SUB_CATEGORY(name,main_code) values('해외여행',4);
+insert into SUB_CATEGORY(code,name,main_code) values(410,'국내여행',400);
+insert into SUB_CATEGORY(code,name,main_code) values(420,'해외여행',400);
 
-insert into SUB_CATEGORY(name,main_code) values('IT비즈니스',5);
-insert into SUB_CATEGORY(name,main_code) values('개발/프로그래밍',5);
-insert into SUB_CATEGORY(name,main_code) values('컴퓨터/앱활용',5);
+insert into SUB_CATEGORY(code,name,main_code) values(510,'IT비즈니스',500);
+insert into SUB_CATEGORY(code,name,main_code) values(520,'개발/프로그래밍',500);
+insert into SUB_CATEGORY(code,name,main_code) values(530,'컴퓨터/앱활용',500);
 
 ---------------------------------------------------------------------------------------
 --book 만들기
 
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('넛지','리처드 탈러, 캐스선스타인','리더북스',2,5);
+values('넛지','리처드 탈러, 캐스선스타인','리더북스',200,220);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('화폐전쟁','쑹훙빈','랜덤하우스코리아',2,5);
+values('화폐전쟁','쑹훙빈','랜덤하우스코리아',200,220);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('그들이 말하지 않는 23가지','장하준','부키',2,5);
+values('그들이 말하지 않는 23가지','장하준','부키',200,220);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('경제학 콘서트','팀 하포드','웅진지식하우스',2,5);
+values('경제학 콘서트','팀 하포드','웅진지식하우스',200,220);
 
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('경영학 콘서트','장영재','비즈니스북스',2,4);
+values('경영학 콘서트','장영재','비즈니스북스',200,210);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('카르마 경영','이나모리가즈오','서돌',2,4);
+values('카르마 경영','이나모리가즈오','서돌',200,210);
 
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('진짜 서울은 골목 안에 있다','이주화','이른 아침',4,10);
+values('진짜 서울은 골목 안에 있다','이주화','이른 아침',400,410);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('지하철로 떠나는 서울&근교 여행','최미선, 신석교','넥서스BOOKS',4,10);
+values('지하철로 떠나는 서울&근교 여행','최미선, 신석교','넥서스BOOKS',400,410);
 insert into BOOK(subject,writer,publisher,main_code,sub_code) 
-values('주말엔 서울여행','유철상','상상출판',4,10);
+values('주말엔 서울여행','유철상','상상출판',400,410);
